bfe8adb8bac2bd9b4e0877c0a3c3ecc4f58b931b
Chanuda2000 <chanuda2000@gmail.com>
2024-09-16 12:08:50 +0530

    Nav bar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+
+   
+      
+    </div>
+  )
+}
+
+export default App
